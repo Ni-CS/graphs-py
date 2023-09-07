85c88c52c24b00c47be536b5ca9384977ed7be72
Ni-CS <nichcs@gmail.com>
2023-09-07 18:16:16 -0300

    First commit

---
+++ department.py
@@ -0,0 +1,6 @@
+class Department:
+
+    def __init__(self, id, name, people):
+        self.id = id
+        self.name = name
+        self.people_amount = people
---
+++ edge.py
@@ -0,0 +1,12 @@
+from department import Department
+
+
+class Edge:
+
+    def __init__(self, dep1, dep2, dist):
+        self.dept1 = dep1
+        self.dept2 = dep2
+        self.dist = dist
+
+    def get_path_info(self):
+        return "Caminho: {} <-----{}-----> {}".format(self.dept1.name, self.dist, self.dept2.name)
